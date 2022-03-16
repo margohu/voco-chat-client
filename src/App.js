04d9b38dc6aacf08f79d49555093e585f1c252c7
Margo Hutt <margo.hutt@ir.ee>
2022-03-16 22:40:27 +0200

    Something went terribly wrong

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container} from "react-bootstrap";
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">VOCO</header>header>
-              <Container className="Text-area">
-              </Container>
-    </div>
-  );
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            messages: [],
+            user: {},
+        };
+    }
+    componentDidMount() {
+        const messages = [
+            {
+                "text":"Lorem",
+                "id": "1",
+                "sender": {
+                    "name": "Aadu",
+                    "uid": "user1",
+                },
+            },
+
+        ];
+        const user = {
+            "uid": "user1"
+        };
+        this.setState({messages: messages, user: user});
+    }
+
+render () {
+    return (
+        <div className="App">
+            <header className="App-header">VOCO</header>
+
+            <Container className="Text-area">
+                <App messages={this.state.messsages}/>
+            </Container>
+        </div>
+        )
+    }
 }
 
 export default App;
